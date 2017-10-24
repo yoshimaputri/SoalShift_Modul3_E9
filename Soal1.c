9ee8642249c317dce8060a2751d1a6ec1ce7b434
jharisonn <john.harison99@gmail.com>
2017-10-25 00:18:38 +0700

    Tinggal beberapa tambahan

--- Soal1.c
+++ Soal1.c
@@ -3,21 +3,44 @@
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <string>
+#include <string.h>
 #include <sys/wait.h>
-char weapon[6][8]={"MP4A1","PM2-V1","SPR-3","SS2-V5","SPG1-V3","MINE"};
-int stock[6]={"19","10","13","0","5","9"};
 int main(){
 	key_t keyp= 1234;
-	int shmid = shmget(keyp,sizeof(int), IPC_CREAT | 0666);
-	if(shmid<0){
+	char weapon[6][8]={"MP4A1", "PM2-V1","SS2-V5","SPR-3","SPG1-V3","MINE"};
+	int shmid = shmget(keyp,sizeof(int)*6, IPC_CREAT | 0666);
+/*	if(shmid<0){
 		perror("shmget");
 		exit(1);
-	}
+	}*/
+	int *shm;
 	shm = shmat(shmid,NULL,0);
-	if(shm == (char*) -1){
-		perror("shmat");
-		exit(1);
+	while(1){
+		printf("1. pilih 1 untuk lihat stock senjata\n");
+		printf("2. pilih 2 untuk menambahkan senjata\n");
+		int pil;
+		scanf("%d",&pil);
+		if(pil==1){
+			for(int i=0;i<6;i++){
+				if(shm[i]>0){
+					printf("Jumlah %s di stock = %d barang\n",weapon[i],shm[i]);
+				}
+			}
+		}
+		if(pil==2){
+			char temp[10];
+			int jmlh;
+			printf("Ketik 'nama barang' 'jumlah stock' \n");
+			scanf("%s %d",temp,&jmlh);
+			//printf("%s\n",temp);
+			for(int i=0;i<6;i++){
+				if(!strcmp(weapon[i],temp)){
+					shm[i]+=jmlh;
+					//printf("berhasil");
+					break;
+				}
+			}
+		}
 	}
 
 }
