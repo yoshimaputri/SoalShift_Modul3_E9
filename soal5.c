bf375127f90ae59f45d2c451d0e3912ddddef682
yoshimaputri <yoshimashima@gmail.com>
2017-10-24 19:12:18 +0000

    nyoba input aja | john kalo push bilang dulu ya, biar ga error dan disuruh pull-push lagi

--- soal5.c
+++ soal5.c
@@ -9,16 +9,22 @@ char kata1[20]; char kata2[20];
 
 void* cari()
 {
+	printf("%s", kata1);
 }
 
 void* carilagi()
 {
+	printf("%s", kata2);
 }
 
 int main()
 {
 	scanf("%s", kata1);
 	scanf("%s", kata2);
+	FILE *path;
+	path=fopen("novel.txt", "r");
+//	size_t length;
+//	printf("%lu", length);
 	pthread_create(&(tid[0]),NULL,&cari,NULL);
 	pthread_create(&(tid[1]),NULL,&carilagi,NULL);
 
