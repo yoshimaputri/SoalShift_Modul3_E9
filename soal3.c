9468cc890ec2b675ffa9a9c2a08587fc20f21a77
jharisonn <john.harison99@gmail.com>
2017-10-24 22:54:36 +0700

    udah ada fungsi belom bener tapi hehe

--- soal3.c
+++ soal3.c
@@ -4,24 +4,46 @@
 #include <unistd.h>
 #include <string.h>
 int kep=100,loh=100;
-pthread_t tid[2];
-void* lohan(){ //thread 1
-	char a;
-	while(kep>0){
-		printf("Berikan makan kepada Lohan(Y/N) ? \n");
-		scanf("%c",a);
-		if(a=="Y" || a=="y") loh+=10;
-		
+pthread_t tid[3];
+void* binatang(){ //thread 1
+	while(kep > 0 && loh > 0){
+		char a,b;
+		printf("Beri makan lohan ?(Y/N) \n");
+		scanf("%c",&a);
+		if(a=='y'||a=='Y') loh+=10;
+		printf("Beri makan kepiting ?(Y/N) \n");
+		scanf("%c",&b);
+		if(b=='y'||b=='Y') kep+=10;
+		sleep(10);
+		printf("Status Kepiting = %d\n",kep);
+		printf("Status Lohan = %d\n",loh);
 	}
+	if(kep<=0) {
+		printf("Game Over Kepiting mati\n");
+		exit(1);
+	}
+	else if(loh<=0){
+		printf("Game Over Lohan Mati\n");
+		exit(1);
+	}
+
 }
-void* kepiting(){ //thread 2
-	while(loh>0){
-
+void* timer(){
+	while(kep>0){
+		sleep(20);
+		kep-=10;
 	}
 
+}
+void* timer1(){
+	while(loh>0){
+		sleep(10);
+		loh-=15;
+	}
 }
 int main(){
-	pthread_create(&(tid[0]),NULL,&lohan,NULL);
-	pthread_create(&(tid[1]),NULL,&kepiting,NULL);
-	for(int i=0;i<2;i++) pthread_join(tid[i],NULL);
+	pthread_create(&(tid[0]),NULL,&binatang,NULL);
+	pthread_create(&(tid[1]),NULL,&timer,NULL);
+	pthread_create(&(tid[2]),NULL,&timer1,NULL);
+	for(int i=0;i<3;i++) pthread_join(tid[i],NULL);
 }
