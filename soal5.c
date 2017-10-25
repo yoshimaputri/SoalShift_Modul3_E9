0d3340acb5b28d60e414eff27ee03b7088608272
yoshimaputri <yoshimashima@gmail.com>
2017-10-25 05:27:58 +0000

    tes input lewat argument dah bener

--- soal5.c
+++ soal5.c
@@ -5,45 +5,27 @@
 #include <string.h>
 
 pthread_t tid[2];
-char kata1[20]; char kata2[20];
-int i;
+int i; FILE *path; int status;
 
-void* cari()
+void* cari(void *arg)
 {
-	printf("%s", kata1);
+	status=0;
+	path=fopen("novel.txt", "r");
+	if(strcmp((char*)arg,"ifah")==0) printf("sip");
+	fclose(path);
+	status=1;
 }
 
 void* carilagi()
 {
-	printf("%s", kata2);
+	while(status!=1) {}
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	scanf("%s", kata1);
-	scanf("%s", kata2);
-	FILE *path; i=0;
-	char *line=NULL;
-//	size_t len=0; ssize_t read;
-	path=fopen("novel.txt", "r");
-//	while(read=getline(&line,&len,path)!=-1){ 
-//	if(fgetc(path)!=EOF){ printf("bs");
-//		while(getline(path,line)){
-//			if(strstr(line,kata1)!=NULL) {printf("nemu "); i++;}
-//	} printf("%d", i);
-char x; string word;
-	while(path!=EOF){
-	x=fgets();
-	while(x!=' '){
-	word=word+x;
-x=fgets();
-}
-printf("%s",word);
-}
-
-	fclose(path);
-	pthread_create(&(tid[0]),NULL,&cari,NULL);
-	pthread_create(&(tid[1]),NULL,&carilagi,NULL);
+	printf("%s", argv[1]);
+	pthread_create(&(tid[0]),NULL,&cari,(void*)argv[1]);
+	pthread_create(&(tid[1]),NULL,&carilagi,(void*)argv[2]);
 
 	pthread_join(tid[0],NULL);
 	pthread_join(tid[1],NULL);
