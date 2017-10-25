18fb8f23c70b7363745aa0924ddb9cf947058c17
jharisonn <john.harison99@gmail.com>
2017-10-25 13:40:55 +0700

    baru sedikit.. tar lanjut lagi yaa berfikir dulu saya

--- soal2.c
+++ soal2.c
@@ -4,12 +4,29 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-pthread_t tid1;
-pthread_t tid2;
+pthread_t tid[2];
+char player1[50],player2[50];
+int score=0,score1=0;
+char board[17][17];
+void* player(){
+	pthread_t id=pthread_self();
+	if(pthread_equal(id,tid[0])){
 
+	}
+	else if(pthread_equal(id,tid[1])){
+
+	}
+}
 int main(){
-	pthread_create(&(tid1),NULL,&p1,NULL);
-	pthread_create(&(tid2),NULL,&p2,NULL);
-	pthread_join(tid1,NULL);
-	pthread_join(tid2,NULL);
+	while(1){
+		printf("Masukkan nama untuk Player 1:\n");
+		scanf("%s",player1);
+		printf("Masukkan nama untuk Player 2:\n");
+		scanf("%s",player2);
+		for(int i=0;i<16;i++){
+			for(int j=0;j<16;j++){
+				board[i][j]=0;
+			}
+		}
+	}
 }
