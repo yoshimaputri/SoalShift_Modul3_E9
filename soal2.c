56f559754cb4f871828093720e0d164fd5a11610
yoshimaputri <yoshimashima@gmail.com>
2017-10-25 07:33:22 +0000

    soal2.c modified | pemain 1 : ngisi lubang,ngasih tebakan

--- soal2.c
+++ soal2.c
@@ -6,16 +6,28 @@
 
 pthread_t tid[2];
 char player1[50],player2[50];
-int score1=0,score2=0;
-char board[17];
+int score1=0,score2=0; int tebak[4];
+int board1[17]; int board2[17];
+int in,x,nolubang;
+
 void* p1(){
-	printf("p1 : berapa lubang yg akan diisi ranjau?");
-	scanf("%d", &i);
-	while(i--){
-	printf("p1 : lubang mana yg akan diisi? [no_lubang]");
-	scanf("%d", &nolubang);
-	board[nolubang]=1;
+	printf("p1 : berapa lubang yg akan diisi ranjau? ");
+	scanf("%d", &x);
+	printf("p1 : lubang mana yg akan diisi? ");
+	while(x--) {
+		scanf("%d", &nolubang);
+		board1[nolubang]=1;
 	}
+	printf("\np1 beri tebakan pada p2\nada empat lubang yg aman\nlubang manakah itu?\n");
+	printf("p2 menjawab : ");
+	scanf("%d %d %d %d",&tebak[0],&tebak[1],&tebak[2],&tebak[3]);
+	for(int b=0;b<4;b++){
+		if(board1[tebak[b]]==1)//jika jawaban p2 salah
+			score1+=1;
+		else score2+=1; //jika jawaban p2 benar
+	}
+	printf("score p1 now : %d\n",score1);
+	printf("score p2 now : %d\n", score2);
 }
 void* p2(){
 }
@@ -25,15 +37,21 @@ int main(){
 	printf("Masukkan nama untuk Player 2:\n");
 	scanf("%s",player2);
 	for(int i=0;i<16;i++){
-		board[i]=0;
+		board1[i]=0; board2[i]=0;
 	}
+	while(1){
 	pthread_create(&(tid[0]),NULL,&p1,NULL);
 	pthread_create(&(tid[1]),NULL,&p2,NULL);
-	printf("fitur list :\n1. cek status\n");
-	while(1){
-		scanf("%d",&in);
-		printf("player 1 : %d\n", score1);
-		printf("player 2 : %d\n", score2);
-	}
-	pthread_join(tid[0]);
+	if(score1>=10) {printf("p1 menang. skor : %d\n", score1); break;}
+	if(score2>=10) {printf("p2 menang. skor : %d\n", score2); break;}
+	
+}
+	pthread_join(tid[0],NULL);
+	pthread_join(tid[1],NULL);
+//	printf("fitur list :\n1. cek status\n");
+//	while(1){
+//		scanf("%d",&in);
+//		printf("player 1 : %d\n", score1);
+//		printf("player 2 : %d\n", score2);
+//	}
 }
